af9d82d93a2fbce6f4c0170babc9d30465778415
Mathis Rouy <c1120@cnetp.fr>
2025-02-21 17:26:32 +0100

    Changement du nom du dossier

---
+++ app/(pages)/contactez-nous/page.tsx
@@ -0,0 +1,15 @@
+'use client'; 
+
+import Header from "@/app/_components/header";
+import { useState } from "react";
+
+export default function contactUs() {
+    return (
+        <Header />
+
+
+
+
+        
+    );
+}
