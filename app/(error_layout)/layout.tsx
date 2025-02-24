8e614bb029975d099e8ea2a34c85e939a7fdcb74
Mathis Rouy <c1120@cnetp.fr>
2025-02-24 14:12:10 +0100

    Modification de la langue dans la balise <html> + Ajout de l'import de la fontawesome

--- app/(error_layout)/layout.tsx
+++ app/(error_layout)/layout.tsx
@@ -1,19 +1,24 @@
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: '404',
-  description: 'Cette pag est introuvable'
-}
+// FONTAWESOME
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
+// export const metadata: Metadata = {
+//     title: '404',
+//     description: 'Cette pag est introuvable'
+// }
 
 export default function ErrorLayout() {
-  return (
-    <html lang="fr">
-      <head>
-        <title>Erreur | Mon Site</title>
-      </head>
-      <body>
-        <main></main> {/* Ce layout ne dépend pas du parent */}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <title>Erreur | Mon Site</title>
+            </head>
+            <body>
+                <main></main> {/* Ce layout ne dépend pas du parent */}
+            </body>
+        </html>
+    );
 }
