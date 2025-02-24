362d161d8ea9b3502c64c79599d2a51ec067e055
Mathis Rouy <c1120@cnetp.fr>
2025-02-24 16:44:26 +0100

    Mise en page dans la page contact

--- app/ui/contacez-nous.tsx
+++ app/ui/contacez-nous.tsx
@@ -7,31 +7,64 @@ export default function ContactezNous() {
 
     return (
 
-        <div className="border-2 w-full h-auto text-white">
-            <h1 className="text-4xl text-center">Conctatez-Nous</h1>
-            <form className="grid grid-cols-1 text-white">
+        <div className="w-full h-auto text-white">
+            <h1 className="text-3xl text-center">Contactez-Nous</h1>
+            <form className="grid grid-cols-1 text-white mt-8">
                 <div className="flex justify-center gap-48 mb-10">
-                    <input className="w-72 py-2.5 px-4 bg--form rounded-md placeholder-[#8C5744] placeholder-opacity-75" type="email" name="" id="" placeholder="Votre Email" />
-                    <input className="w-72 py-2.5 px-4 bg--form rounded-md 
+                    <input className="
+                        w-72 
+                        py-2.5 
+                        px-4
+                        bg--form 
+                        rounded-md 
+                        outline-none
+                      placeholder-[#8C5744] placeholder-opacity-75
+                        focus:ring-2 focus:ring-[#C29A7E]
+                        transition"
+                        type="email"
+                        placeholder="Votre Email"
+                    />
+
+
+                    <input className="
+                    w-72 
+                    py-2.5 
+                    px-4 
+                    bg--form 
+                    rounded-md 
                     placeholder-[#8C5744]
-                    focus:border-[#8C5744]
-                    focus:ring-2 focus:ring-[#c07a61]
+                    focus:border-[#c07a61]
+                    focus:ring-2 focus:ring-[#C29A7E]
                     outline-none
-                    transition" 
-                    
-                    type="number" name="" id="" placeholder="Votre Téléphone" />
+                    transition"
+                        type="number" name="" id="" placeholder="Votre Téléphone" />
                 </div>
 
-                <div className="border-1 border-green-600 flex justify-center">
-                    <textarea className="w-2/5 h-72 bg--form placeholder-[#8C5744] rounded-xs" name="" id="" placeholder="Votre message"></textarea>
+                {/* TEXTAREA */}
+                <div className="flex justify-center">
+                    <textarea className="
+                    w-2/5 
+                    h-72
+                    pl-1
+                    pt-1
+                    bg--form 
+                    placeholder-[#8C5744] 
+                    rounded-md
+                    outline-none
+                    focus:border-[#c07a61]
+                    focus:ring-2 focus:ring-[#C29A7E]"
+                        name="" id="" placeholder="Votre message"></textarea>
                 </div>
 
-                <div className="">
-                    <button className="" type="submit">Envoyer</button>
-
-                    <button className="ml-64 bg-violet-500 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700 ...">
-                        Save changes
-                    </button>
+                <div className="w-full flex justify-center mt-6">
+                    <button className="
+                    rounded-md
+                    py-2.5 
+                    px-4
+                    mb-16
+                    bg--form
+                    hover"
+                        type="submit" id="">Contactez-nous</button>
                 </div>
 
             </form>
