68e24dfeec34dc96dd13585ca7535d654c3e99a6
Mathis Rouy <c1120@cnetp.fr>
2025-02-24 17:23:45 +0100

    Ajout de la balise <head> et des information dedans + Gestion dynamique du titre de la page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+'use client';
+import { usePathname } from "next/navigation"; 
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/app/styles/globals.css";
 
@@ -8,8 +11,24 @@ config.autoAddCss = false;
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 
+  
+    const router = usePathname();
+  
+    // Récupération de l'URL && On split l'url en 2 && On récupère le deuxième élément de la chaine de caractère 
+    const myUrl = router;
+    const pathnameUrl = myUrl.split("/");
+    const titlePage = pathnameUrl[1] || "Accueil";
+  
+    console.log(titlePage);
+
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet" />
+        <title>{titlePage}</title>
+      </head>
       <body>
         <main>{children}</main>
       </body>
