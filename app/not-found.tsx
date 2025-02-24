2c01b8aac08869d2681ee6476a53e188123e372d
Mathis Rouy <c1120@cnetp.fr>
2025-02-24 14:12:39 +0100

    UPDATE

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -13,18 +13,22 @@ export const metadata: Metadata = {
 
 export default function NotFound() {
   return (
-    <div className='flex flex-col justify-center items-center text-white gap-3'>
-      <Image
-        src="/logo/logoTNS-white.png"
-        width={250}
-        height={100}
-        alt="Logo blanc de l'application"
-      />
-      <h2>Erreur 404: Cette page est sortie de la route !</h2>
-      <p>Nos développeurs essayent de la retrouver... ou peut-être pas. <FontAwesomeIcon icon={faTruck} /></p>
-      <Link href="/" className="underline underline-offset-2 hover:text-[#8C5744] transition duration-300">
-        Page d&apos;accueil
-      </Link>
-    </div>
+
+    <React.Fragment>
+      <div className='flex flex-col justify-center items-center text-white gap-3'>
+        <Image
+          src="/logo/logoTNS-white.png"
+          width={250}
+          height={100}
+          alt="Logo blanc de l'application"
+        />
+        <h2>Erreur 404: Cette page est sortie de la route !</h2>
+        <p>Nos développeurs essayent de la retrouver... ou peut-être pas. <FontAwesomeIcon icon={faTruck} /></p>
+        <Link href="/" className="underline underline-offset-2 hover:text-[#8C5744] transition duration-300">
+          Page d&apos;accueil
+        </Link>
+      </div>
+    </React.Fragment>
+
   )
 }
