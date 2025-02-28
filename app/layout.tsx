7bc80d0b100ddd69bf9912964cbd4c818e5e26ad
Mathis Rouy <c1120@cnetp.fr>
2025-02-28 17:15:09 +0100

    Retirage de l'import fontawesome

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 'use client';
+
 import { usePathname } from "next/navigation"; 
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/app/styles/globals.css";
 
-// FontAwesome
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-config.autoAddCss = false;
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-
   
     const router = usePathname();
   
