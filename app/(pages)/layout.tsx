631dd78acb2183d9617d28faaea79c35d20561fb
Mathis Rouy <c1120@cnetp.fr>
2025-03-03 17:31:44 +0100

    Suppression de la gestion dynamique de la gestion du titre

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,17 +1,8 @@
-"use client";
 
 import Header from "../_components/header";
 import Footer from "../_components/footer";
-import { usePathname } from "next/navigation";
 
 export default function DefaultLayout({ children }: { children: React.ReactNode }) {
-
-  const router = usePathname();
-
-  // Récupération de l'URL && On split l'url en 2 && On récupère le deuxième élément de la chaine de caractère 
-  const myUrl = router;
-  const pathnameUrl = myUrl.split("/");
-  const titlePage = pathnameUrl[1] || "Accueil";
 
   return (
     <main>
