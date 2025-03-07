05d8ee36df05e5d6d7d3c6d38a7e08c64eb08115
Mathis Rouy <c1120@cnetp.fr>
2025-03-07 16:38:13 +0100

    UPDATE

--- app/ui/monCompte.tsx
+++ app/ui/monCompte.tsx
@@ -4,21 +4,21 @@ import { useState } from "react";
 
 export default function MonCompte() {
     return (
-        <section className="border-2 border-red-500 h-auto w-full text-white">
+        <section className="w-full text-white">
             <h1 className="text-3xl text-center md:w-1/4 md:text-2xl md:text-center md:pl-8">Mon compte</h1>
-            <div className="border flex flex-col justify-center items-center gap-8 md:flex-row md:justify-around md:items-start">
-                <div className="border-1 border-purple-500 rounded-md px-36 py-9 mt-3 bg--form">
+            <div className=" flex flex-col justify-center items-center gap-8 md:flex-row md:justify-around md:items-start">
+                <div className="rounded-md px-36 py-9 mt-3 bg--form">
                     <p className="mb-2">Mes Informations</p>
                     <p>Mes réservations</p>
                 </div>
 
-                <form className="border-1 border-blue-500 rounded-md bg--form grid grid-cols-2 gap-4 py-9 px-2 
-                md:2/4 md:grid md:grid-cols-3 " action="">
+                <form className="rounded-md bg--form grid grid-cols-2 gap-4 py-9 px-7 mb-[305px] 
+                md:2/4 md:grid md:grid-cols-3" action="">
 
                     {/* NOM */}
                     <div className="w-40 ml-7 md:w-48 md:ml-0">
                         <input type="text"
-                            className="form-control border-none md:text-sm md:h-9"
+                            className="form-control border-none md:text-sm md:h-10"
                             name=""
                             id=""
                             value={"Rouy"}
@@ -27,31 +27,36 @@ export default function MonCompte() {
 
                     {/* PRENOM */}
                     <div className="w-40 md:w-48">
-                        <input className="form-control border-none md:text-sm md:h-9" type="text" name="" id="" value={"Mathis"}/>
+                        <input className="form-control border-none text-sm md:text-sm md:h-10" type="text" name="" id="" value={"Mathis"}/>
                     </div>
 
 
                     {/* N° Téléphone */}
                     <div className="w-40 ml-7 md:w-48 md:ml-0">
-                        <input className="form-control border-none" type="number" name="" id="" value={"0616250830"} />
+                        <input className="form-control border-none text-sm h-10" type="number" name="" id="" value={"0616250830"} />
                     </div>
+
 
                     {/* EMAIL */}
-                    <div className="w-40 md:ml-0 md:pr-20 md:pl-20 ">
-                        <input className="form-control" type="email" name="" value={"mrouy"} />
+                    <div className="w-40 
+                    md:ml-0 md:w-56">
+                        <input className="form-control border-none text-sm h-10" type="email" name="" />
                     </div>
 
+
                     {/* MOT DE PASSE */}
-                    <div className="w-full col-span-full px-7 md:ml-0 md:pr-20 md:pl-20 ">
-                        <input className="form-control " type="text" name="" value={"caefdzefzefzefzef"}/>
+                    <div className="w-full col-span-full px-7
+                    md:ml-0 md:w-1/2 md:px-0 md:col-span-2">
+                        <input className="form-control border-none text-sm h-10" type="password" name="" value={"caefdzefzefzefzef"}/>
                     </div>
 
 
                     <div className="col-span-full px-7 flex justify-end gap-2">
                         <button type="reset" className="
                         btn-submit
-                        py-2.5 
-                        px-4
+                        py-2 
+                        px-3
+                        text-sm
                         font-semibold
                         bg--grisArdoise
                         hover:bg-[#2a2835]">
@@ -60,9 +65,9 @@ export default function MonCompte() {
 
                         <button type="submit" className="
                         btn-submit 
-                        py-2.5 
-                        px-4 
-                        text-base
+                        py-2 
+                        px-3 
+                        text-sm
                         font-semibold
                         text-[#8C5744] 
                         bg-[#FBFBFB]                         
