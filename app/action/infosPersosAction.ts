a248ec8c20e462e652968d1c5c604f4a2fe3acd0
Mathis Rouy <c1120@cnetp.fr>
2025-03-10 10:58:29 +0100

    Ajout des fichiers qui gèrent chacun 1 composant de la page profil

---
+++ app/action/infosPersosAction.ts
@@ -0,0 +1,9 @@
+'use server';
+
+import pool from "../_lib/db";
+import { useState } from "react";
+import { console } from "inspector";
+
+export const function getPersonalInfo(FormData: FormData) {
+     
+}
