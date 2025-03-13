37ec8738f255824ec7dc4244e1dc05800801c182
HumanM00n <mrouy92@gmail.com>
2025-03-13 09:53:50 +0100

    Ajout du composant MesRservations. Reste a faire la condition d'affichage

--- app/(pages)/profil/page.tsx
+++ app/(pages)/profil/page.tsx
@@ -1,7 +1,11 @@
-import MesInfos from "@/app/ui/monCompte";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import PersonalInfoForm from "@/app/ui/monCompte";
+import MyBookings from "@/app/ui/mesRervations";
 
 export default function page() {
     return (
-        <MesInfos />
+        // <PersonalInfoForm/>
+        <MyBookings />
+
     );
 }
