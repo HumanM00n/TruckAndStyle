ec31cc3c207b7dc27193e06f1282b701a0c94ab9
HumanM00n <mrouy92@gmail.com>
2025-03-16 09:39:55 +0100

    Début du développment de la page d'accueil

--- app/ui/accueil.tsx
+++ app/ui/accueil.tsx
@@ -1,5 +1,10 @@
-'use client'
+'use client';
 
-export default function Accueil() {
-    return 'bienvenue'
+export default function Home() {
+    return (
+        <section className="border-1 border-blue-600">
+            
+        </section>
+
+    )
 }
