d1fa3182de79493af7442a748fffd8b0d37079fc
Mathis Rouy <c1120@cnetp.fr>
2025-03-19 16:34:48 +0100

    Création d'une page de test. Refonte de la page inscription de l'utilisateur pour le responsive

---
+++ app/(pages)/testDeveloppement/page.tsx
@@ -0,0 +1,164 @@
+'use client';
+
+import Link from "next/link";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
+
+
+export default function page() {
+
+    const [showPassword, setshowPassword] = useState(false);
+
+    return (
+        <>
+            <div className="
+            
+            w-full 
+            h-auto 
+            flex 
+            justify-center
+            pb-32
+            md:min-h-auto
+            md:pb-0
+            ">
+                <form className="
+                 
+                w-[600px]
+                grid-cols-1 
+                bg--form 
+                px-10 
+                py-9
+                rounded-md 
+                md:mt-10
+                md:translate-x-1/2
+                md:z-50
+                md:max-h-[550px]">
+                    <h1 className="text-white text-center mb-4 text-2xl">Inscription</h1>
+
+                    {/* NOM & PRENOM */}
+                    <div className=" flex justify-center gap-4 mb-4">
+                        <input className="
+                        form-control 
+                        text-[15px]
+                        placeholder-[#8C5744] placeholder-opacity-70 focus:ring-[#8C5744] focus:border-[#8C5744] focus:ring-4 transition" 
+                        type="text" 
+                        placeholder="Nom" />
+
+                        <input className="
+                        form-control 
+                        text-[15px]
+                        placeholder-[#8C5744] placeholder-opacity-70 focus:ring-[#8C5744] focus:border-[#8C5744] focus:ring-4 transition" 
+                        type="text" 
+                        placeholder="Prénom" />
+                    </div>
+
+                    {/* N° TEL & DDN */}
+                    <div className=" flex justify-center gap-4 mb-4">
+                        <input className="
+                        form-control 
+                        text-[15px]
+                        placeholder-[#8C5744] placeholder-opacity-70 focus:ring-[#8C5744] focus:border-[#8C5744] focus:ring-4 transition" 
+                        type="tel" 
+                        id="" 
+                        placeholder="N° Tel" />
+
+                        <input className="
+                        form-control 
+                        text-[15px]
+                        placeholder-[#8C5744] placeholder-opacity-70 focus:ring-[#8C5744] focus:border-[#8C5744] focus:ring-4 transition" 
+                        type="date" 
+                        data-placeholder="Date de naissance"
+                        id="dateNaissance" />
+                    </div>
+
+                    {/* EMAIL */}
+                    <div className=" flex justify-center gap-4 mb-4">
+                        <input className="
+                        form-control 
+                        text-[15px]
+                        placeholder-[#8C5744] placeholder-opacity-70 focus:ring-[#8C5744] focus:border-[#8C5744] focus:ring-4 transition" 
+                        type="email" 
+                        placeholder="Email" 
+                        id="" />
+                    </div>
+
+                    <div className="relative w-full mb-4">
+                        <input
+                            type={showPassword ? "text" : "password"}
+                            className="form-control 
+                            text-[15px] 
+                            w-full  
+                            rounded-md 
+                            border border-gray-300 
+                            placeholder-[#8C5744] placeholder-opacity-70 focus:ring-[#8C5744] focus:border-[#8C5744] focus:ring-4 transition"
+                            placeholder="Mot de passe"
+                        />
+
+                        <FontAwesomeIcon
+                            icon={showPassword ? faEye : faEyeSlash}
+                            className="
+                            w-5 
+                            h-5 
+                            absolute 
+                            right-3 
+                            top-1/2 transform -translate-y-1/2 
+                            cursor-pointer
+                            placeholder-[#8C5744] placeholder-opacity-70 focus:ring-[#8C5744] focus:border-[#8C5744] focus:ring-4 transition"
+                            onClick={() => setshowPassword(!showPassword)}
+                        />
+                    </div>
+
+
+                    {/* DEPARTEMENT & CP */}
+                    <div className=" flex justify-center gap-4 mb-4">
+                        <input className="
+                        form-control 
+                        text-[15px]
+                        placeholder-[#8C5744] placeholder-opacity-70 focus:ring-[#8C5744] focus:border-[#8C5744] focus:ring-4 transition" 
+                        type="text" 
+                        // value={}
+                        id=""
+                        placeholder="Département" />
+
+                        <input className="
+                        form-control 
+                        text-[15px]
+                        placeholder-[#8C5744] placeholder-opacity-70 focus:ring-[#8C5744] focus:border-[#8C5744] focus:ring-4 transition" 
+                        type="number" 
+                        placeholder="Code Postal" />
+                    </div>
+
+                    {/* VILLE */}
+                    <div className="flex justify-center gap-4 mb-4">
+                        <input className="form-control 
+                        text-[15px]
+                        placeholder-[#8C5744] placeholder-opacity-70 focus:ring-[#8C5744] focus:border-[#8C5744] focus:ring-4 transition" 
+                        type="text" 
+                        placeholder="Ville" />
+                    </div>
+
+                    {/* BTN */}
+                    <div className="flex justify-center gap-4 mb-4 text-white">
+                        <button className="btn btn-outline-light text-[15px] rounded-xs hover:bg-white hover:text-[#733E34]" type="submit">Créer mon compte</button>
+                    </div>
+                </form>
+
+                <div className="
+                hidden 
+                md:rounded-md
+                md:relative
+                md:left-40
+                md:w-2/5
+                md:block">
+                    <img
+                    src="/assets/photoTondeuse.jpg"
+                    className="w-full h-[650px] object-cover"
+                    />
+
+                </div>
+            </div>
+        </>
+
+    )
+}
