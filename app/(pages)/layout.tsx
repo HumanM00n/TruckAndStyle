d7c77b0b796ec7a1f9bdaa53c5f8fc5e992303d6
Mathis Rouy <c1120@cnetp.fr>
2025-03-24 14:10:54 +0100

    Replacement de l'appel du composant Providers

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -3,12 +3,13 @@ import Header from "../_components/header";
 import Footer from "../_components/footer";
 
 export default function DefaultLayout({ children }: { children: React.ReactNode }) {
-
   return (
-    <main>
-      <Header />
-        <Providers><section className="bg--noir ">{children}</section></Providers>
-      <Footer />
-    </main>
+    <Providers>
+      <main>
+        <Header />
+        <section className="bg--noir">{children}</section>
+        <Footer />
+      </main>
+    </Providers>
   );
 }
