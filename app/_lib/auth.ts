e8e30d499a7875bd2987f8cdbff44d88987cd0f9
Mathis Rouy <c1120@cnetp.fr>
2025-03-26 18:39:25 +0100

    Composant qui permet de vérifier le token

---
+++ app/_lib/auth.ts
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+
+export const verifyToken = (token: string) => {
+
+    const secret = process.env.JWT_SECRET;
+
+    if (!secret) {
+        throw new Error('La clé JWT n\'est pas définie dans les variables d\'environnement');
+    }
+
+    try {
+        const decoded = jwt.verify(token, secret);
+        return decoded;
+
+    } catch (error) {
+        return null;
+    };
+}
