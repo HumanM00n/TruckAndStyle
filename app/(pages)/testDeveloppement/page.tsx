dedb1e8677019330c7e987169185cb14f1c90ccd
Mathis Rouy <c1120@cnetp.fr>
2025-04-10 09:37:14 +0200

    Changement de la couleur des boutons du carousel

--- app/(pages)/testDeveloppement/page.tsx
+++ app/(pages)/testDeveloppement/page.tsx
@@ -1,111 +1,69 @@
 'use client';
 
-import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
+
 
 export default function page() {
 
-
     return (
         <>
-            <h1 className="text-center text-2xl">
-                Notre <span className="color--form ">Truck&apos;s</span>
-            </h1>
-            <div className="border flex flex-col">
-                {/* IMAGE DU TRUCK */}
-                <div className=""></div>
+            <div id="carouselExampleDark" className="carousel carousel-light slide border-1 border-blue-500 min-h-[600px]" data-bs-ride="carousel">
 
-                <div className="">
-                    <h2>Le hair&apos;truck</h2>
+                {/* Indicateurs */}
+                <div className="carousel-indicators mb-0">
+                    <button
+                        type="button"
+                        data-bs-target="#carouselExampleDark"
+                        data-bs-slide-to="0"
+                        className="active"
+                        aria-current="true"
+                        aria-label="Slide 1"
+                    />
+                    <button
+                        type="button"
+                        data-bs-target="#carouselExampleDark"
+                        data-bs-slide-to="1"
+                        aria-label="Slide 2"
+                        className=""
+                    />
+                    <button
+                        type="button"
+                        data-bs-target="#carouselExampleDark"
+                        data-bs-slide-to="2"
+                        aria-label="Slide 3"
+                        className=""
+                    />
                 </div>
 
-                <div className="">
-                    <p>
-                        {" "}
-                        Praesent ante libero, congue eu ante a, faucibus euismod lorem. In
-                        sit amet mauris non tellus laoreet porttitor. Aenean congue
-                        tincidunt rutrum. Etiam bibendum pharetra egestas. Sed sagittis
-                        rutrum tortor, sollicitudin vulputate ex finibus a. Aenean mauris
-                        magna, imperdiet quis ornare vel, finibus a erat. Vestibulum sem
-                        odio, placerat eu nunc ut, dictum malesuada nisl. Sed eget libero
-                        sed enim maximus porttitor.
-                    </p>
-                </div>
-            </div>
-
-            {/* Liste des coiffures */}
-            <div className="border-1 border-green-500 flex justify-center">
-                <div className="border grid grid-cols-2 gap-6">
-
-                    {/* CARD 1 */}
-                    <div className="border card flex justify-center min-w-48 max-w-52 max-h-52 bg--form pt-3 cursor-pointer">
-                        <Image src={"/assets/buzz-cut.jpg"}
-                            alt="Coupe courte buzz cut"
-                            width={150}
-                            height={50}
-                            quality={90}
-                            className="rounded-lg h-36 ml-5"
-                        />
-
-                        <div className="card-body">
-                            <p className="text-sm text-white text-center">Coupes courtes</p>
+                {/* Slides */}
+                <div className="carousel-inner border h-[450px]">
+                    <div className="carousel-item active border-1 border-red-500 h-[450px]" data-bs-interval="10000">
+                        <FontAwesomeIcon icon={faCircleUser} className="text-5xl text-[#733E34] " />
+                        <div className="carousel-caption pb-0 position-none d-none d-md-block text-white border-1 border-yellow-400">
+                            <h5>First slide label</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
                         </div>
                     </div>
 
-                    {/* CARD 2 */}
-                    <div className="card min-w-48 max-w-52 max-h-52 bg--form pl-3 pt-2 cursor-pointer">
-                        <Image src={"/assets/mi-long.jpg"}
-                            alt="Coupe courte buzz cut"
-                            width={140}
-                            height={50}
-                            quality={90}
-                            className="rounded-lg max-h-[150px] ml-3"
-
-                        />
-                        <div className="card-body">
-                            <p className="text-sm text-white px-0 ">Coupes mi-longues</p>
+                    <div className="carousel-item" data-bs-interval="2000">
+                        <FontAwesomeIcon icon={faCircleUser} className="text-5xl text-white" />
+                        <div className="carousel-caption d-none d-md-block text-white mt-32">
+                            <h5>Second slide label</h5>
+                            <p>Some representative placeholder content for the second slide.</p>
                         </div>
                     </div>
 
-
-                    {/* CARD 3 */}
-                    <div className="card min-w-48 max-w-52 max-h-52 bg--form pl-3 pt-2 cursor-pointer">
-                        <Image src={"/assets/degrade.jpg"}
-                            alt="Coupe courte buzz cut"
-                            width={150}
-                            height={50}
-                            quality={90}
-                            className="rounded-lg h-36 ml-2"
-
-                        />
-                        <div className="card-body">
-                            <p className="text-sm text-white px-0 ">Coupes dégradé</p>
-                        </div>
-                    </div>
-
-                    {/* CARD 4 */}
-                    <div className="card min-w-48 max-w-52 max-h-52 bg--form pl-3 pt-2 cursor-pointer">
-                        <Image src={"/assets/drop-fade.jpg"}
-                            alt="Coupe courte buzz cut"
-                            width={150}
-                            height={50}
-                            quality={90}
-                            className="rounded-lg h-36 ml-2"
-
-                        />
-                        <div className="card-body">
-                            <p className="text-sm text-white px-0 ">Coupes tendances</p>
+                    <div className="carousel-item">
+                        <FontAwesomeIcon icon={faCircleUser} className="text-5xl text-white" />
+                        <div className="carousel-caption d-none d-md-block text-white">
+                            <h5>Third slide label</h5>
+                            <p>Some representative placeholder content for the third slide.</p>
                         </div>
                     </div>
                 </div>
             </div>
-
-            <div className="border-red-500 bg-[#1F1F1F] w-full">
-                <div className="">
-
-                </div>
-            </div>
-
         </>
 
     )
