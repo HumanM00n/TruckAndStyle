cae462dc71ed63bcb15a3beab453b5b340a98049
Mathis Rouy <c1120@cnetp.fr>
2025-04-10 17:21:24 +0200

    Test du carousel

--- app/(pages)/testDeveloppement/page.tsx
+++ app/(pages)/testDeveloppement/page.tsx
@@ -2,14 +2,12 @@
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
-import Image from "next/image";
-
 
 export default function page() {
 
     return (
         <>
-            <div id="carouselExampleDark" className="carousel carousel-light slide border-1 border-blue-500 min-h-[600px]" data-bs-ride="carousel">
+            <div id="carouselExampleDark" className="carousel carousel-light slide border-1 border-blue-500 w-full min-h-[470px] px-9" data-bs-ride="carousel">
 
                 {/* Indicateurs */}
                 <div className="carousel-indicators mb-0">
@@ -38,28 +36,36 @@ export default function page() {
                 </div>
 
                 {/* Slides */}
-                <div className="carousel-inner border h-[450px]">
-                    <div className="carousel-item active border-1 border-red-500 h-[450px]" data-bs-interval="10000">
-                        <FontAwesomeIcon icon={faCircleUser} className="text-5xl text-[#733E34] " />
-                        <div className="carousel-caption pb-0 position-none d-none d-md-block text-white border-1 border-yellow-400">
-                            <h5>First slide label</h5>
-                            <p>Some representative placeholder content for the first slide.</p>
+                <div className="carousel-inner h-[340px] w-full border-2 mt-2 px-6 pt-2 pb-4">
+
+                    <div className="carousel-item active h-[300px] border-1 border-purple-500 bg--form rounded-lg" data-bs-interval="10000">
+                        <FontAwesomeIcon icon={faCircleUser} className="text-5xl text-white pl-4 pt-3" />
+                        <div className="carousel-caption rounded-lg h-1/2  d-md-block pb-0 position-absolute bottom-16 text-white text-left px-3">
+                            <p className="object-fill">“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et nisl ex. Suspendisse erat augue, maximus at dui at, pulvinar ullamcorper ante.”</p>
+                            <div className="max-w-[500px] flex justify-end">
+                                <span className="font-bold">Alex.D</span>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className="carousel-item h-[300px] border-1 border-purple-500" data-bs-interval="2000">
+                        <FontAwesomeIcon icon={faCircleUser} className="text-5xl text-[#733E34] pl-4 pt-3" />
+                        <div className="carousel-caption rounded-lg h-1/2 d-md-block pb-0 position-absolute bottom-16 text-white text-left px-3">
+                            <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et nisl ex. Suspendisse erat augue, maximus at dui at, pulvinar ullamcorper ante.”</p>
+                            <div className="max-w-[500px] flex justify-end">
+                                <span className="font-bold">Mathis.R</span>
+                            </div>
                         </div>
                     </div>
 
-                    <div className="carousel-item" data-bs-interval="2000">
-                        <FontAwesomeIcon icon={faCircleUser} className="text-5xl text-white" />
-                        <div className="carousel-caption d-none d-md-block text-white mt-32">
-                            <h5>Second slide label</h5>
-                            <p>Some representative placeholder content for the second slide.</p>
-                        </div>
-                    </div>
-
-                    <div className="carousel-item">
-                        <FontAwesomeIcon icon={faCircleUser} className="text-5xl text-white" />
-                        <div className="carousel-caption d-none d-md-block text-white">
-                            <h5>Third slide label</h5>
-                            <p>Some representative placeholder content for the third slide.</p>
+                    <div className="carousel-item h-[300px] border-1 border-purple-500" data-bs-interval="3000">
+                        <FontAwesomeIcon icon={faCircleUser} className="text-5xl text-[#733E34] pl-4 pt-3" />
+                        <div className="carousel-caption rounded-lg h-1/2 d-md-block pb-0 position-absolute bottom-16 text-white text-left px-3">
+                            <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et nisl ex. Suspendisse erat augue, maximus at dui at, pulvinar ullamcorper ante.”</p>
+                            <div className="max-w-[500px] flex justify-end ">
+                                <span className="font-bold">Myriam.JD</span>
+                            </div>
                         </div>
                     </div>
                 </div>
