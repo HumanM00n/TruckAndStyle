2bfee241ca0f8ce3776e492468bbbd07ede092e6
Mathis Rouy <c1120@cnetp.fr>
2025-04-14 15:55:36 +0200

    Développement de la première section terminée

--- app/_components/accueil/ps-information.tsx
+++ app/_components/accueil/ps-information.tsx
@@ -1,16 +1,23 @@
 "use client";
+import Image from "next/image";
 
 export default function PsInformations() {
     return (
-        <section className="border-1 border-blue-600 h-auto w-auto">
+        <section className="h-auto w-auto">
 
             {/* PREMIRE SECTION */}
-            <div className="text-white flex flex-col mb-3 min-h-dvh">
-                <div className="w-full px-3">
-                    <h1 className="text-3xl mb-3"> Bienvenue sur <span className="color--form">Truck&apos;n Style</span></h1>
+            <div className="text-white flex flex-col mb-3 min-h-dvh
+            md:flex
+            md:w-full
+            md:flex-row
+            md:justify-around">
+                <div className="w-full px-3
+                md:w-1/2">
+                    <h1 className="text-3xl mb-3
+                    md:text-4xl"> Bienvenue sur le site <br /> <span className="color--form">Truck&apos;n Style</span></h1>
 
-                    <p className="text-sm">
-                        orem ipsum dolor sit amet, consectetur adipiscing elit. Duis et nisl
+                    <p className="text-md lg:text-lg">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et nisl
                         ex. Suspendisse erat augue, maximus at dui at, pulvinar ullamcorper
                         ante. Integer dapibus ligula mi, sed mattis nisi rutrum vitae. Nam
                         in leo non nunc commodo convallis in non enim. Pellentesque interdum
@@ -36,7 +43,50 @@ export default function PsInformations() {
                     </div>
                 </div>
 
-                <div className="hidden"></div>
+                {/* PARTIE DROITE */}
+                <div className="hidden 
+                [@media(max-width:1607px)]:hidden
+                md:flex
+                md:flex-row-reverse
+                md:w-1/2
+                md:h-full
+                md:static">
+
+                    <div className="w-[450px] absolute top-56 right-1/4 p-10 translate-x-8 z-10 bg--form rounded-md
+                    ">
+                        <p className="text-center">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et nisl ex. Suspendisse erat augue, maximus at dui at, pulvinar ullamcorper ante.
+                            Integer dapibus ligula mi, sed mattis nisi rutrum vitae. Nam in leo non nunc commodo convallis in non enim.
+                        </p>
+                    </div>
+
+                    {/* TONDEUSE */}
+                    <div className="mr-16 z-0">
+                        <Image
+                            src={"/assets/objets_coiffure/tondeuse2.png"}
+                            alt="Photo d'une personne qui se fait coiffer la barbe"
+                            height={450}
+                            width={550}
+                            quality={75}
+                            className=""
+                        />
+                    </div>
+
+                    <div className="z-10 absolute right-1/4 -bottom-8 translate-x-8">
+                        <Image
+                            src={"/assets/Coiffures/photo-coiff-accueil.jpg"}
+                            alt="Photo d'une personne qui se fait coiffer la barbe"
+                            height={200}
+                            width={350}
+                            quality={75}
+                            className="rounded-md"
+                            style={{
+
+                                boxShadow: '-60px -20px 20px 10px #733E34',
+                            }}
+                        />
+                    </div>
+                </div>
             </div>
         </section>
     );
