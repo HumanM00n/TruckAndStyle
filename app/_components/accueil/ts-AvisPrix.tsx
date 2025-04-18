ffeba98b603b63dd3cae99893758792d32a4319c
Mathis Rouy <c1120@cnetp.fr>
2025-04-18 17:15:44 +0200

    UPDATE

--- app/_components/accueil/ts-AvisPrix.tsx
+++ app/_components/accueil/ts-AvisPrix.tsx
@@ -2,12 +2,27 @@
 
 import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRef } from "react";
 
 export default function TsAvisPrix() {
 
+    const scrollCard = useRef<HTMLDivElement>(null);
+
+    // const scroll
+
     return (
         <section className="border-1 border-blue-500 w-full min-h-auto bg--grisArdoiseMid ">
-                
+                <div className="border flex flex-col justify-center text-white">
+
+                    {/* NOS PRIX */}
+                    <div className="">
+                        
+                    </div>
+
+                    {/* COMMENTAIRES */}
+                    <div className=""></div>
+
+                </div>
         </section>  
     );
 }
