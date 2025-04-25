b5a7eb59e934ff97459218b66cb202fee302d811
Mathis Rouy <c1120@cnetp.fr>
2025-04-25 17:23:33 +0200

    Switch d'affichage des différents composants

--- app/(pages)/testDeveloppement/page.tsx
+++ app/(pages)/testDeveloppement/page.tsx
@@ -3,14 +3,16 @@
 import PsInformations from "@/app/_components/accueil/ps-information";
 import DsInfosCoupes from "@/app/_components/accueil/ds-informationsCoupes";
 import TsAvisPrix from "@/app/_components/accueil/ts-AvisPrix";
+import QsFaq from "@/app/_components/accueil/qs-faq";
 
 export default function page() {
 
     return (
         <>
-            {/* <PsInformations /> */}
-            {/* <DsInfosCoupes /> */}
+            <PsInformations />
+            <DsInfosCoupes />
             <TsAvisPrix />
+            <QsFaq />
         </>
     );
 }
