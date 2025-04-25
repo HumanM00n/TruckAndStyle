7016fb9a19375846a04553e3cc2eb357a0db0bfa
Mathis Rouy <c1120@cnetp.fr>
2025-04-25 08:52:36 +0200

    Suppression des sauts de ligne inutile et ce qui permet d'améliorer le SEO

--- app/_components/accueil/ts-AvisPrix.tsx
+++ app/_components/accueil/ts-AvisPrix.tsx
@@ -11,32 +11,28 @@ import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 import typesCoupes from '@/app/_lib/typeCoupes';
 
 export default function TsAvisPrix() {
-
     // Création d'un tableau d'avis 
     const avisUtilisateurs = [
         {
             nom: 'Alex.D',
             texte: '“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et nisl ex...”'
         },
-
         {
             nom: 'Mathis.R',
             texte: '“Suspendisse erat augue, maximus at dui at, pulvinar ullamcorper ante.”'
         },
-
         {
             nom: 'Myriam.JD',
             texte: '“Duis et nisl ex. Suspendisse erat augue, maximus at dui at...”'
         },
     ]
-
     return (
         <section className="w-full min-h-auto">
             <div className="flex flex-col justify-center text-white mt-14
             lg:flex-row-reverse
             lg:justify-between
             lg:mt-28">
-
+                
                 {/* NOS PRIX */}
                 <div className="w-full relative mt-8 mb-4 px-4 h-auto
                 lg:min-h-max
@@ -45,7 +41,6 @@ export default function TsAvisPrix() {
                 ">
                     <h2 className="text-2xl">Nos <span className="color--form">Prix</span></h2>
                     <div className="mt-2 font-montserrat">
-
                         {typesCoupes.map((group, content) => (
                             <Accordion key={content} className='bg-transparent text-white !shadow-[0px_2px_1px_0px_rgba(115,62,52,0.44)]'>
                                 <AccordionSummary
@@ -66,23 +61,17 @@ export default function TsAvisPrix() {
                                             <span className="text-sm">{group.cout}</span>
                                     </AccordionDetails>
                                 ))}
-
                             </Accordion>
-
                         ))}
-
                     </div>
                 </div>
 
                 {/* COMMENTAIRES */}
                 <div className="w-full h-auto mt-3">
                     <h2 className='text-2xl pl-5 lg:pl-16'>Vos <span className="color--form">Avis</span></h2>
-
                     <div id="carouselExampleDark" className="carousel carousel-light slide min-h-[400px] px-9" data-bs-ride="carousel">
-
                         {/* Indicateurs */}
                         <div className="carousel-indicators mb-28">
-
                             {avisUtilisateurs.map((_, index) =>( 
                                 <button
                                     key={index}
@@ -94,10 +83,7 @@ export default function TsAvisPrix() {
                                     aria-label={`Slide ${index + 1}`}
                                 />
                             ))}
-
-                        </div>
-
-                        
+                        </div>                        
                         <div className="carousel-inner h-[340px] w-full mt-2 px-6 pt-2 pb-4">
                             {avisUtilisateurs.map((item, i) => (
                                 <div
@@ -115,10 +101,8 @@ export default function TsAvisPrix() {
                                 </div>
                             ))}
                         </div>
-
                     </div>
                 </div>
-
             </div>
         </section>
     );
