9fc40db44945d2d1629e4d0ffa88aa81837e7af4
Mathis Rouy <c1120@cnetp.fr>
2025-05-16 17:31:06 +0200

    Fichier data pour les types de coupes lors de la réservation

---
+++ app/_lib/choixCoupes.ps-resa.ts
@@ -0,0 +1,34 @@
+// app/data/dataCoupes.ts
+
+export type Coupe = {
+  coupes: string;
+  cout: string;
+  temps: string;
+};
+
+export type View = 'courtes' | 'mi-longues' | 'degrades' | 'tendances';
+
+export const dataMap: Record<View, Coupe[]> = {
+  courtes: [
+    { coupes: 'Buzz cut', cout: '10 €', temps: '30 minutes' },
+    { coupes: 'Crew cut', cout: '10 €', temps: '30 minutes' },
+    { coupes: 'French crop', cout: '10 €', temps: '30 minutes' },
+    { coupes: 'High and Tight', cout: '10 €', temps: '30 minutes' },
+  ],
+  'mi-longues': [
+    { coupes: 'Slide Back', cout: '10 €', temps: '30 minutes' },
+    { coupes: 'Slide Part', cout: '10 €', temps: '30 minutes' },
+    { coupes: 'Middle Part', cout: '10 €', temps: '30 minutes' },
+  ],
+  degrades: [
+    { coupes: 'High, Mid, Low Fade', cout: '10 €', temps: '30 minutes' },
+    { coupes: 'Drop Fade', cout: '10 €', temps: '30 minutes' },
+    { coupes: 'Brust Fade', cout: '10 €', temps: '30 minutes' },
+    { coupes: 'Dégradé Espagnol', cout: '10 €', temps: '30 minutes' },
+  ],
+  tendances: [
+    { coupes: 'French Crop', cout: '10 €', temps: '30 minutes' },
+    { coupes: 'Taper', cout: '10 €', temps: '30 minutes' },
+    { coupes: 'High Top Fade', cout: '10 €', temps: '30 minutes' },
+  ],
+};
