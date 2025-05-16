0d399a12aa7a2de7b3158f8fe2854add88ff1836
Mathis Rouy <c1120@cnetp.fr>
2025-05-16 17:32:46 +0200

    Test de composant

--- app/_components/test.tsx
+++ app/_components/test.tsx
@@ -1,22 +1,55 @@
 'use client';
 
-import { useSession } from "next-auth/react";
+import { useState } from "react";
 
-export default function TestSession() {
-    const { data: session, status } = useSession();
+type Coupe = {
+  coupes: string;
+  cout: string;
+  temps: string;
+};
 
-    if (status === "loading") {
-        return <p>Chargement...</p>;
-    }
+
+type CoupeDataMap = {
+    [key: string]: Coupe[];
+}
+
+type Props = {
+    data: CoupeDataMap;
+} 
+
+
+export default function DynamicDisplay({ data }: Props) {
+
+  const [view, setView] = useState<string>('courtes');
+
+  const boutons = Object.keys(data);
+
+  const coupesActuelles = data[view];
 
     return (
-        <div className="text-white">
-            <h1>Test de Session</h1>
-            {session ? (
-                <pre>{JSON.stringify(session, null, 2)}</pre>
-            ) : (
-                <p>Pas de session active.</p>
-            )}
+        <div className="p-4">
+            <div className="flex gap-4 mb-6">
+                {boutons.map((key) => (
+                    <button
+                        key={key}
+                        onClick={() => setView(key)}
+                        className={`px-4 py-2 rounded ${view === key ? 'bg-blue-600 text-white' : 'bg-gray-200'
+                            }`}
+                    >
+                        {key}
+                    </button>
+                ))}
+            </div>
+
+            <ul className="space-y-4">
+                {coupesActuelles.map((item, index) => (
+                    <li key={index} className="p-4 border rounded shadow">
+                        <p className="font-bold">{item.coupes}</p>
+                        <p>Prix : {item.cout}</p>
+                        <p>Durée : {item.temps}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
