16fddcd8be65a8e6b080a10fac7586850959f6dd
Mathis Rouy <c1120@cnetp.fr>
2025-05-16 10:27:13 +0200

    Responsive terminé pour les boutons cadre de choix de coupes

--- app/_components/reservation/ps-choixCoupes.tsx
+++ app/_components/reservation/ps-choixCoupes.tsx
@@ -1,18 +1,33 @@
-import { Button } from "@mui/material";
+'use client';
+
+import { useSession } from "next-auth/react";
 import Image from "next/image";
-// import { useRef } from "react";
+import { useState, useEffect } from "react";
 
 export default function PsChoixCoupes() {
+
+    const [viewShortCuts, setViewShortCuts] = useState(true);
+    const [viewMidLenghtCuts, setViewMidLenghtCuts] = useState(false);
+    const [viewDegradedCuts, setDegradedCuts] = useState(false);
+    const [viewTrendCuts, setTrendCuts] = useState(false);
+
     return (
         <section className="border-1 border-blue-500 w-auto h-auto text-white">
-            <div className="border flex flex-column">
+            <div className="flex flex-column">
 
                 <div className="">
-                    <h1 className="text-2xl text-center">Le choix <span className="color--form">de la coupes</span></h1>
+                    <h1 className="text-3xl text-center">Le choix <span className="color--form">de la coupes</span></h1>
                 </div>
 
-                <div className="w-full grid grid-cols-2 gap-4 px-4">
-                    <div className="border-1 border-[#733E34] w-52 px-2 py-3 mt-3 rounded-lg">
+                <div className="w-full grid grid-cols-2 gap-4 px-4 mb-3
+                md:flex
+                md:text-sm
+                lg:text-base
+                lg:px-0
+                lg:justify-center
+                ">
+
+                    <div className="border-1 border-[#733E34] w-52 px-2 py-3 mt-3 rounded-lg transition duration-300 hover:scale-105">
                         <Image
                             alt="Image d'une coupe de cheveux"
                             src={"/assets/photoCadreResa.png"}
@@ -20,10 +35,37 @@ export default function PsChoixCoupes() {
                             height={150}
                             quality={70}
                             className="w-full mb-3"
-
                         />
-                        <span className="ml-8">Coupes courtes</span>
+                        <span className="relative left-7 md:left-4">Coupes courtes</span>
+                    </div>
 
+                    <div className="border-1 border-[#733E34] w-52 px-2 py-3 mt-3 rounded-lg transition duration-300 hover:scale-105">
+                        <button onClick={() => setViewMidLenghtCuts(false)}>
+                            <Image
+                            alt="Image d'une coupe de cheveux"
+                            src={"/assets/photoCadreResa.png"}
+                            width={150}
+                            height={150}
+                            quality={70}
+                            className="w-full mb-3"
+                        />
+                        <span className="relative">Coupes mi-longues</span>
+                        </button>
+
+                    </div>
+
+                    <div className="border-1 border-[#733E34] w-52 px-2 py-3 mt-3 rounded-lg transition duration-300 hover:scale-105">
+                        <button onClick={() => setDegradedCuts(false)}>
+                            <Image
+                                alt="Image d'une coupe de cheveux"
+                                src={"/assets/photoCadreResa.png"}
+                                width={150}
+                                height={150}
+                                quality={70}
+                                className="w-full mb-3"
+                            />
+                            <span className="relative">Coupes dégardés</span>
+                        </button>
                     </div>
 
                     <div className="border-1 border-[#733E34] w-52 px-2 py-3 mt-3 rounded-lg transition duration-300 hover:scale-105">
@@ -35,37 +77,11 @@ export default function PsChoixCoupes() {
                             quality={70}
                             className="w-full mb-3"
                         />
-                        <span className="ml-4">Coupes mi-longues</span>
-
+                        <span className="relative left-5 md:left-2">Coupes tendances</span>
                     </div>
-
-
-                    <button className="border-1 border-[#733E34] w-52 px-2 py-3 mt-3 rounded-lg">
-                        <Image
-                            alt="Image d'une coupe de cheveux"
-                            src={"/assets/photoCadreResa.png"}
-                            width={150}
-                            height={150}
-                            quality={70}
-                            className="w-full mb-3"
-                        />
-                        <span className="">Coupes dégardés</span>
-                    </button>
-
-                    <div className="border-1 border-[#733E34] w-52 px-2 py-3 mt-3 rounded-lg">
-                        <Image
-                            alt="Image d'une coupe de cheveux"
-                            src={"/assets/photoCadreResa.png"}
-                            width={150}
-                            height={150}
-                            quality={70}
-                            className="w-full mb-3"
-                            id=""
-                        />
-                        <span className="ml-5">Coupes tendances</span>
-                    </div>
-
                 </div>
+
+                {/* <div className=""></div> */}
             </div>
         </section>
     );
