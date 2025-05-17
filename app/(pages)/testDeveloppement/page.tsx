6c3a61963266dca1e5eacd77d798cfb60c14337c
HumanM00n <mrouy92@gmail.com>
2025-05-17 03:16:05 +0200

    Update de dev

--- app/(pages)/testDeveloppement/page.tsx
+++ app/(pages)/testDeveloppement/page.tsx
@@ -1,33 +1,8 @@
-'use client';
-
-import { useState } from "react";
-import { useSession } from "next-auth/react";
-import PersonalInfoForm from "@/app/ui/mesInfosPersonnelles";
-import MyBookings from "@/app/ui/mesReservations";
-import { redirect } from "next/navigation";
+"use client";
+import DynamicDisplay from "@/app/_components/test";
 
 export default function MonCompte() {
-    const { data, status } = useSession();
-
-    console.log(data)
-
-    const [mesInfosPersos, setMesInfosPersos] = useState(true);
-
-    if (status === "authenticated") {
-        return (
-            <section className="border-1 border-blue-500 w-full h-[680px] text-white md:min-h-[390px]">
-                <h1 className="text-3xl text-center md:w-1/4 md:text-2xl md:text-center md:pl-8">Mon compte</h1>
-                <div className="border flex flex-col justify-center items-center gap-8 lg:flex-row md:justify-around md:items-start">
-                    <div className="border-1 border-red-500 flex flex-col rounded-md px-36 py-9 mt-3 bg--form">
-                        <button className="mb-2 transition-transform duration-200 hover:scale-105 hover:shadow-xl" onClick={() => setMesInfosPersos(true)}>Mes Informations</button>
-                        <button className="transition-transform duration-200 hover:scale-105 hover:shadow-xl" onClick={() => setMesInfosPersos(false)}>Mes réservations</button>
-                    </div>
-                    {mesInfosPersos ? <PersonalInfoForm /> : <MyBookings />}
-
-                </div>
-            </section>
-        );
-    } else {
-        // redirect('/') 
-    }
+  return (
+    <DynamicDisplay />
+  );
 }
--- app/_components/reservation/ps-choixCoupes.tsx
+++ app/_components/reservation/ps-choixCoupes.tsx
@@ -2,20 +2,20 @@
 
 import Image from "next/image";
 import { useState } from "react";
-import { dataMap, Coupe, View } from "@/app/_lib/choixCoupes.ps-resa";
+import { dataMap, View } from "@/app/_lib/choixCoupes.ps-resa";
 
 export default function PsChoixCoupes() {
     const [selected, setSelected] = useState<View | null>('courtes');
 
     return (
-        <section className="border-1 border-blue-500 w-auto h-auto text-white">
+        <section className="border-1 border-blue-500 w-auto min-h-[700px] text-white">
             <div className="flex flex-column">
 
                 <div className="">
                     <h1 className="text-3xl text-center">Le choix <span className="color--form">de la coupes</span></h1>
                 </div>
 
-                <div className="w-full grid grid-cols-2 gap-4 px-4 mb-3
+                <div className="w-full grid grid-cols-2 gap-4 px-4 
                 md:flex
                 md:text-sm
                 lg:text-base
@@ -44,14 +44,23 @@ export default function PsChoixCoupes() {
                 </div>
 
                 {selected && (
-                    <div className="border w-full max-w-lg p-4">
+                    <div className="w-full p-4
+                    md:grid grid-cols-2 gap-12
+                    md:relative
+                    md:top-10
+                    xl:gap-x-72
+                    2xl:border
+                    2xl:w-1/2
+                    2xl:relative
+                    2xl:left-1/4
+                    ">
                         {dataMap[selected].map((item, index) => (
-                            <div key={index} className="mb-4 p-4 border rounded-md">
-                                <div className="flex justify-between mb-2">
+                            <div key={index} className="mb-4 border-none border-bottom">
+                                <div className="flex justify-between mb-4">
                                     <span className="font-bold">{item.coupes}</span>
                                     <span>{item.cout}</span>
                                 </div>
-                                <div className="flex justify-between items-center">
+                                <div className="flex justify-between items-center pb-2">
                                     <span>{item.temps}</span>
                                     <button className="btn text-sm text-white rounded bg-[#733E34] px-3 py-1 hover:bg-[#733e3471]">
                                         Réserver cette coupe
