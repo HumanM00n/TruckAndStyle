f8506ade8b374ec2e33d0b2fb7afe811c34a6d09
Mathis Rouy <c1120@cnetp.fr>
2025-05-21 15:53:41 +0200

    Import de la deuxième partie de la page reservation

--- app/ui/reservation.tsx
+++ app/ui/reservation.tsx
@@ -1,7 +1,11 @@
 import PsChoixCoupes from "../_components/reservation/ps-choixCoupes";
+import DsHoraireAndTruck from "../_components/reservation/ds-horaireTruck";
 
 export default function Reservation() {
   return (
-    <PsChoixCoupes />
+    <>
+      <PsChoixCoupes />
+      <DsHoraireAndTruck />
+    </>
   );
 }
