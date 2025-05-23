dedcc01b679d9fb3e959257c0b00c02493b256e7
Mathis Rouy <c1120@cnetp.fr>
2025-05-23 15:51:29 +0200

    Ajout des fonts montserrat et playfair

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 'use client';
 
 import { usePathname } from "next/navigation";
-
+import { montserrat, playfair } from '@/app/styles/fonts';
 import "bootstrap/dist/css/bootstrap.min.css";
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "@/app/styles/globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -17,17 +16,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   return (
 
-    <html lang="en">
+    <html lang="en" className={`${montserrat.variable} ${playfair.variable}`}>
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet" />
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossOrigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.min.js" integrity="sha384-VQqxDN0EQCkWoxt/0vsQvZswzTHUVOImccYmSyhJTp7kGtPed0Qcx8rK9h9YEgx+" crossOrigin="anonymous"></script>
 
         <title>{titlePage}</title>
       </head>
-      <body>
+      <body className="font-montserrat text-white">
         {children}
       </body>
     </html>
