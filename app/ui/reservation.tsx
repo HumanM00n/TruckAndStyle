987cdc979ac426a0f4f07a332dc9929e96fb32db
Mathis Rouy <c1120@cnetp.fr>
2025-05-23 15:52:28 +0200

    Ajout des composant de la page

--- app/ui/reservation.tsx
+++ app/ui/reservation.tsx
@@ -1,11 +1,21 @@
+'use client';
+import { useState } from "react";
 import PsChoixCoupes from "../_components/reservation/ps-choixCoupes";
-import DsHoraireAndTruck from "../_components/reservation/ds-horaireTruck";
+import DsHoraireAndTruckResa from "../_components/reservation/ds-horaireTruck";
+import TsChoixHeureResa from "../_components/reservation/ts-choixHeure";
+import { View } from "../_lib/choixCoupes.ps-resa";
 
 export default function Reservation() {
+  // const [coupe, setCoupe] = useState<string | null>('Courtes');
+  const [horaire, setHoraire] = useState<string | null>(null);
+  const [coupe, setCoupe] = useState<View | null>('courtes');
+  
+
   return (
     <>
-      <PsChoixCoupes />
-      {/* <DsHoraireAndTruck /> */}
+      {/* <PsChoixCoupes selected={coupe} onSelect={setCoupe} /> */}
+      {/* <DsHoraireAndTruckResa /> */}
+      <TsChoixHeureResa />
     </>
   );
 }
