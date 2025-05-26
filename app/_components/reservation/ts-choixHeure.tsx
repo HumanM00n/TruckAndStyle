7a916dec9fadb9197313d8d0e86dce33a5f7cbb0
Mathis Rouy <c1120@cnetp.fr>
2025-05-26 16:19:59 +0200

    Modifications Graphique : - UPDATE - Ajout d'éléments HTML
    Modifications fonctionnels :
    - Ajout de Props
    - Foncnalitée de reset des infos de la réservation

--- app/_components/reservation/ts-choixHeure.tsx
+++ app/_components/reservation/ts-choixHeure.tsx
@@ -1,26 +1,41 @@
 'use client';
 
 import Image from "next/image";
+import DateTimePicker from "@/app/_components/test"
 
-export default function TsChoixHeureResa() {
+type Props= {
+    date: Date | undefined;
+    setDate: (value: Date | undefined) => void;
+
+    heure: string | null;
+    setHeure: (value: string | null) => void;
+
+    affichage: string;
+    setAffichage: (value: string) => void;
+}
+
+export default function TsChoixHeureResa({date, setDate, heure, setHeure, affichage, setAffichage}: Props) {
+
+    const handleDateTime = (datetime: string) => {
+        console.log("Créneau choisi :", datetime)
+    }
+
+    const resetReservation = () => {
+        setDate(undefined);
+        setHeure(null);
+        setAffichage("Choisir une date")
+    }
+    
+
     return (
         <section className="border-1 border-blue-500 w-auto min-h-[80vh] text-white">
             <div className="border flex flex-col">
-                <h2 className=" font-montserrat text-2xl">Choix de votre <span className="color--form">Horaire</span></h2>
-
                 <div className=" font-bold">
-                <h2 className="!font-playfair text-2xl">Choix de votre <span className="color--form">Horaire</span></h2>
-
+                    <h2 className="!font-playfair text-2xl">Choix de votre <span className="color--form">Horaire</span></h2>
                 </div>
 
-                <div className="flex gap-4 p-8 bg-gray-900 text-white">
-  <p className="font-montserrat text-2xl">Montserrat par défaut</p>
-  <p className="font-playfair text-2xl">Playfair appliqué ici</p>
-</div>
-
-
                 <div className="border-1 border-[#733E34] w-52 px-2 py-3 mt-3 rounded-lg text-sm mx-auto">
-                    <Image 
+                    <Image
                         alt="Photo de la coiffure selectionnée"
                         src={"/assets/photoCadreResa.png"}
                         width={150}
@@ -45,12 +60,26 @@ export default function TsChoixHeureResa() {
                     </div>
 
                     <div className="text-center">
-                        <button className="cursor-pointer underline">Supprimer</button>
+                        <button className="cursor-pointer underline" onClick={resetReservation}>Supprimer</button>
                     </div>
 
                 </div>
 
-                <div className="">
+                <div className="border-1 border-red-500">
+                    <div className="">
+                        <p className="text-lg">Nom du Hair'Truck</p>
+                        <p className="italic text-sm">*HairOnWhells*</p>
+                    </div>
+
+                    <div className="">
+                        <p>Adresse du Hair'Truck</p>
+                        <p className="italic text-sm">*adresse du hair'Truck*</p>
+                    </div>
+
+                    <div className="p-6 max-w-xl mx-auto">
+                        <DateTimePicker onDateTimeChange={handleDateTime} />
+                    </div>
+
 
                 </div>
             </div>
