172a8d399e5165072997045874cf05a899936cae
Mathis Rouy <c1120@cnetp.fr>
2025-05-26 16:18:20 +0200

    Ajout des Props en State pour les différents composant de la page

--- app/ui/reservation.tsx
+++ app/ui/reservation.tsx
@@ -6,16 +6,25 @@ import TsChoixHeureResa from "../_components/reservation/ts-choixHeure";
 import { View } from "../_lib/choixCoupes.ps-resa";
 
 export default function Reservation() {
-  // const [coupe, setCoupe] = useState<string | null>('Courtes');
-  const [horaire, setHoraire] = useState<string | null>(null);
   const [coupe, setCoupe] = useState<View | null>('courtes');
+  const [coupeChoisie, setCoupeChoisie] = useState<string | null>(null);
+
+  const [date, setDate] = useState<Date | undefined>()
+  const [heure, setHeure] = useState<string | null>(null)
+  const [affichage, setAffichage] = useState<string>("Choisir une date")
   
 
   return (
-    <>
-      {/* <PsChoixCoupes selected={coupe} onSelect={setCoupe} /> */}
+    <form>
+      <PsChoixCoupes selected={coupe} onSelect={setCoupe} onChoixFinal={setCoupeChoisie}/>
       {/* <DsHoraireAndTruckResa /> */}
-      <TsChoixHeureResa />
-    </>
+      <TsChoixHeureResa 
+        date={date}
+        setDate={setDate}
+        heure={heure}
+        setHeure={setHeure}
+        affichage={affichage}
+        setAffichage={setAffichage}/>
+    </form>
   );
 }
