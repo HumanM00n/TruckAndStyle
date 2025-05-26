f2937ac88025a90247252409453082490fa096e2
Mathis Rouy <c1120@cnetp.fr>
2025-05-26 17:23:54 +0200

    Ajout du bouton submit

--- app/ui/reservation.tsx
+++ app/ui/reservation.tsx
@@ -12,19 +12,37 @@ export default function Reservation() {
   const [date, setDate] = useState<Date | undefined>()
   const [heure, setHeure] = useState<string | null>(null)
   const [affichage, setAffichage] = useState<string>("Choisir une date")
-  
+
 
   return (
-    <form>
-      <PsChoixCoupes selected={coupe} onSelect={setCoupe} onChoixFinal={setCoupeChoisie}/>
+    <form className="">
+      {/* <PsChoixCoupes selected={coupe} onSelect={setCoupe} onChoixFinal={setCoupeChoisie} /> */}
       {/* <DsHoraireAndTruckResa /> */}
-      <TsChoixHeureResa 
+      <TsChoixHeureResa
         date={date}
         setDate={setDate}
         heure={heure}
         setHeure={setHeure}
         affichage={affichage}
-        setAffichage={setAffichage}/>
+        setAffichage={setAffichage} />
+
+      <div className=" border-1 border-blue-500 static flex justify-center">
+        <button className="
+                    border
+                    absolute
+                    bottom-0
+                    rounded-md
+                    font-base
+                    w-64
+                    py-2.5 
+                    px-6
+                    mb-16
+                    bg--form
+                    hover:bg-[#63362d]
+                    transition"
+          type="submit" name="">Confirmer</button>
+      </div>
     </form>
+
   );
 }
