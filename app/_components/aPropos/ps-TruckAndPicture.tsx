c4c95a76c513193748a22ed21e03a910a7810e17
Mathis Rouy <c1120@cnetp.fr>
2025-06-05 11:16:14 +0200

    Finalisation graphique de la première partie de la page A Propos

--- app/_components/aPropos/ps-TruckAndPicture.tsx
+++ app/_components/aPropos/ps-TruckAndPicture.tsx
@@ -10,9 +10,9 @@ export default function PsTruckAndPicture() {
 
         /* SECTION NOTRE TRUCK + COUPES PROPOSEES  */
         <section className="min-h-[80vh] text-white">
-            <div className=" w-full">
-                <div className=" border flex justify-center md:w-1/2 md:justify-start md:pl-9 lg:justify-center">
-                    <h1 className="text-3xl font-semibold">Notre <span className="color--form">Truck's</span></h1>
+            <div className="w-full">
+                <div className="flex justify-center md:w-1/2 md:justify-start md:pl-9 lg:pl-10 xl:pl-11">
+                    <h1 className="text-2xl md:text-3xl font-semibold">Notre <span className="color--form">Truck</span></h1>
                 </div>
 
                 {/* IMAGE DU CAMION + LIBELLE */}
@@ -21,9 +21,9 @@ export default function PsTruckAndPicture() {
                     {/* A FAIRE PLUS TARD */}
 
                     {/* <div className="w-auto absolute origin-center rotate-[45deg]  -top-[3.8rem]"> */}
-                        {/* <FontAwesomeIcon icon={faPlay} /> */}
-                        {/* border-1 border-pink-500  */}
-                        {/* <FontAwesomeIcon icon={faCaretLeft} size="10x" className="border-1 border-pink-500 absolute right-[402px] bottom-[133px] text-[20rem] rotate-[45deg] z-10 color--form" /> */}
+                    {/* <FontAwesomeIcon icon={faPlay} /> */}
+                    {/* border-1 border-pink-500  */}
+                    {/* <FontAwesomeIcon icon={faCaretLeft} size="10x" className="border-1 border-pink-500 absolute right-[402px] bottom-[133px] text-[20rem] rotate-[45deg] z-10 color--form" /> */}
                     {/* </div> */}
 
                     {/* border-1 border-purple-500 */}
@@ -42,11 +42,15 @@ export default function PsTruckAndPicture() {
                         <div className="text-center mt-3">
 
                             <h2 className="font-playfair text-xl font-[550] mb-2">Le hair&apos;truck</h2>
-                            <p className="capitalize text-left text-sm">Praesent ante libero, congue eu ante a, faucibus euismod lorem. In sit amet mauris non tellus laoreet porttitor. Aenean congue tincidunt rutrum.
-                                Etiam bibendum pharetra egestas. Sed sagittis rutrum tortor, sollicitudin vulputate ex finibus a. Aenean mauris magna, imperdiet quis ornare vel, finibus a erat.
-                                Vestibulum sem odio, placerat eu nunc ut, dictum malesuada nisl. Sed eget libero sed enim maximus porttitor.</p>
+                            <p className="text-left text-sm"> Le hair’truck, c’est un salon de coiffure mobile pensé pour celles et ceux qui veulent un service professionnel sans avoir 
+                                à se déplacer. Que ce soit à domicile, sur le lieu de travail ou lors d’un événement, Truck’n Style propose des prestations rapides, soignées et adaptées à
+                                ton emploi du temps. Une solution pratique et flexible pour rester bien coiffé, même avec un planning chargé.</p>
                         </div>
                     </div>
+                </div>
+
+                <div className="flex justify-center mt-5 md:justify-start md:pl-9 lg:pl-10 xl:pl-11 2xl:justify-center">
+                    <h2 className="text-2xl md:text-3xl font-semibold">Les coupes <span className="color--form">proposées</span></h2>
                 </div>
 
                 {/* IMAGES DES COUPES */}
