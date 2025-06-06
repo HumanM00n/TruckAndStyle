e9eb09559c5141b55680f5cc1012162ce6a4395c
Mathis Rouy <c1120@cnetp.fr>
2025-06-06 09:13:34 +0200

    Test d'application de la font playfair display

--- app/_components/aPropos/ds-gallerie.tsx
+++ app/_components/aPropos/ds-gallerie.tsx
@@ -1,29 +1,30 @@
 'use client';
+import { playfair } from "@/app/styles/fonts";
+import clsx from "clsx";
 import Image from "next/image";
 
 export default function DsGallerie() {
     return (
         <section className="w-auto min-h-[80vh] bg--grisArdoise text-white px-2 py-4">
             <div className="flex justify-center md:justify-start">
-                <h2 className="text-3xl font-semibold mt-3 mb-4 pl-5 lg:pl-10">La <span className="color--form">Gallerie</span></h2>
+                <h2 className={clsx(playfair.variable, "text-3xl mt-3 mb-4 font-semibold pl-5 lg:pl-10")}>La <span className="color--form">Gallerie</span></h2>
+                {/* <h2 className={"text-3xl mt-3 mb-4 pl-5 lg:pl-10"}>La <span className="color--form">Gallerie</span></h2> */}
             </div>
             <div className="flex justify-center flex-row">
 
                 <div className="">
-                    <Image src="/assets/photoCadreResa.png" alt="Photo 1" width={300} height={200} className="object-cover px-1 w-72 h-40" />
-                    <Image src="/assets/photoCadreResa.png" alt="Photo 2" width={300} height={200} className="object-cover px-1 w-72 h-[325px] mt-2" />
+                    <Image src="/assets/photoCadreResa.png" alt="Photo 1" width={300} height={200} className="object-cover px-1 w-72 h-40 transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg" />
+                    <Image src="/assets/photoCadreResa.png" alt="Photo 2" width={300} height={200} className="object-cover px-1 w-72 h-[325px] mt-2 transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg" />
                 </div>
 
-
                 <div className="">
-                    <Image src="/assets/photoCadreResa.png" alt="Photo centrale plus grande" width={300} height={300} className="object-cover px-1 w-72 h-80" />
-                    <Image src="/assets/photoCadreResa.png" alt="Photo en dessous" width={300} height={200} className="object-cover px-1 w-72 h-[164px]"/>
+                    <Image src="/assets/photoCadreResa.png" alt="Photo centrale plus grande" width={300} height={300} className="object-cover px-1 w-72 h-80 transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg" />
+                    <Image src="/assets/photoCadreResa.png" alt="Photo en dessous" width={300} height={200} className="object-cover px-1 w-72 h-[164px] transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg"/>
                 </div>
 
-
                 <div className="">
-                    <Image src="/assets/photoCadreResa.png" alt="Photo 5" width={300} height={200} className="object-cover px-1 w-72 h-40" />
-                    <Image src="/assets/photoCadreResa.png" alt="Photo 6" width={300} height={200} className="object-cover px-1 w-72 h-[325px] mt-2" />
+                    <Image src="/assets/photoCadreResa.png" alt="Photo 5" width={300} height={200} className="object-cover px-1 w-72 h-40 transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg" />
+                    <Image src="/assets/photoCadreResa.png" alt="Photo 6" width={300} height={200} className="object-cover px-1 w-72 h-[325px] mt-2 transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg" />
                 </div>
             </div>
         </section>
