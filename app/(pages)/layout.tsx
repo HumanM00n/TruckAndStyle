9b08e4b42f585d6b6abe033100245c5503b69ec5
Mathis Rouy <c1120@cnetp.fr>
2025-06-06 11:56:00 +0200

    Ajout des différentes font du projet

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,18 +1,12 @@
 import Providers from "../_components/providers";
 import Header from "../_components/header";
 import Footer from "../_components/footer";
-import { Playfair_Display } from "next/font/google";
-
-const playfair = Playfair_Display({
-  subsets: ["latin"], // ou ["latin-ext"] si besoin
-  weight: ["400", "500", "600", "700"], // les graisses que tu veux utiliser
-  variable: "--font-playfair", // optionnel, si tu veux utiliser en CSS
-})
+import { playfair, montserrat } from "../styles/fonts";
 
 export default function DefaultLayout({ children }: { children: React.ReactNode }) {
   return (
     <Providers>
-      <main>
+      <main className={`${montserrat.variable} ${playfair.variable}`}>
         <Header />
         <section className="bg--noir">{children}</section>
         <Footer />
