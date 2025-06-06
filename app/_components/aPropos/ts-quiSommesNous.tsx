e7639dbeada5780bb168a19a1b49e4c4afbf4b18
Mathis Rouy <c1120@cnetp.fr>
2025-06-06 11:56:24 +0200

    Création du troisième composant de la page A propos

---
+++ app/_components/aPropos/ts-quiSommesNous.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export default function TsQuiSommeNous() {
+    return (
+        <section className="border-1 border-blue-500 w-auto min-h-[80vh] px-4 py-2">
+            <div className=""></div>
+            <div className="">
+                <div className=""></div>
+            </div>
+        </section>
+    )
+} 
