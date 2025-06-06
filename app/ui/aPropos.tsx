b20a30c014f93257d911b06a6aa8fa9e5453b7b7
Mathis Rouy <c1120@cnetp.fr>
2025-06-06 17:22:07 +0200

    Ajout des différents composant de la page

--- app/ui/aPropos.tsx
+++ app/ui/aPropos.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import PsTruckAndPicture from "../_components/aPropos/ps-TruckAndPicture";
+import DsGallerie from "../_components/aPropos/ds-gallerie";
+import TsQuiSommeNous from "../_components/aPropos/ts-quiSommesNous";
 export default function Apropos() {
   return (
     <>
       <PsTruckAndPicture />
+      <DsGallerie />
+      <TsQuiSommeNous />
     </>
   );
 }
