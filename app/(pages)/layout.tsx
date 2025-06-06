33470fbd1ee1d86398538d373c2a37f67350ee1e
Mathis Rouy <c1120@cnetp.fr>
2025-06-06 09:13:11 +0200

    Ajout de la font playfair display

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,6 +1,13 @@
 import Providers from "../_components/providers";
 import Header from "../_components/header";
 import Footer from "../_components/footer";
+import { Playfair_Display } from "next/font/google";
+
+const playfair = Playfair_Display({
+  subsets: ["latin"], // ou ["latin-ext"] si besoin
+  weight: ["400", "500", "600", "700"], // les graisses que tu veux utiliser
+  variable: "--font-playfair", // optionnel, si tu veux utiliser en CSS
+})
 
 export default function DefaultLayout({ children }: { children: React.ReactNode }) {
   return (
