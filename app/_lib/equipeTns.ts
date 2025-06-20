e71cec21f78c71cb166b55379c37ea4e13e086a6
HumanM00n <mrouy92@gmail.com>
2025-06-20 16:08:36 +0200

    Ajout du type photo et informations

--- app/_lib/equipeTns.ts
+++ app/_lib/equipeTns.ts
@@ -2,23 +2,24 @@ export type Presentation = {
     nom: string;
     prenom: string;
     poste: string;
+    photo: string;
+    informations: string;
 }
 
 export type View = "CEO" | "Coiffeur" | "Conducteur";
 
 export const dataEquipe: Record<View, Presentation[]> = {
     CEO: [
-        { nom: 'Rouy', prenom: 'Mathis', poste: "CEO de Hair'Truck" },
-        { nom: "J'Dir", prenom: 'Myriam', poste: "CEO de Hair'Truck" },
-    ],
-    Coiffeur: [
-        { nom: 'Khouri', prenom: "Giulio", poste: "Coiffeur" },
-        { nom: "Furtado", prenom: "Daivy", poste: "Coiffeur" },
-        { nom: "Gounine", prenom: "Quentin", poste: "Coiffeur" },
+        { nom: 'Rouy', prenom: 'Mathis', poste: "CEO de Hair'Truck", photo: "/CEO/Ceo_Mathis.png", informations: "Bonjour, je suis un test" },
+        { nom: "J'Dir", prenom: 'Myriam', poste: "CEO de Hair'Truck", photo: "/CEO/Ceo_Myriam.png", informations: "" },
     ],
     Conducteur: [
-        { nom: "NDoye", prenom: "Moussa", poste: "Conducteur" },
-        { nom: "Bonhomme", prenom: "Simon", poste: "Conducteur" },
+        { nom: "Bonhomme", prenom: "Simon", poste: "Conducteur", photo: "/Conducteur/Conducteur_Simon.png", informations: "" },
+    ],
+    Coiffeur: [
+        { nom: 'Khouri', prenom: "Giulio", poste: "Coiffeur", photo: "/Coiffeurs/Coiffeur_Giulio.png", informations: "" },
+        { nom: "Gounine", prenom: "Quentin", poste: "Coiffeur", photo: "/Coiffeurs/Coiffeur_Quentin.png", informations: "" },
+        { nom: "Furtado", prenom: "Daivy", poste: "Coiffeur", photo: "/Coiffeurs/Coiffeur_Daivy.png", informations: "" },
     ]
 }
 
