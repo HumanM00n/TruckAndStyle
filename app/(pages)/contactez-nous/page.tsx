02febb8a3e199921d1563f58adde14f45d32d180
HumanM00n <mrouy92@gmail.com>
2025-06-20 09:28:08 +0200

    Ajout de l'appel de l'api pour le token csrf + passage en props sur le composant

--- app/(pages)/contactez-nous/page.tsx
+++ app/(pages)/contactez-nous/page.tsx
@@ -1,8 +1,24 @@
+'use client';
 
 import ContactezNous from "@/app/ui/contactez-nous";
+import { useEffect, useState } from "react";
 
-export default function page() {
-    return (
-        <ContactezNous />
-    );
+export default function Page() {
+  const [csrfToken, setCsrfToken] = useState("");
+
+  useEffect(() => {
+    const getToken = async () => {
+      const res = await fetch("/api/csrf", {
+        // Permet de récupérer le cookie 
+        credentials: "include", 
+      });
+      const data = await res.json();
+      setCsrfToken(data.csrfToken);
+    };
+    getToken();
+  }, []);
+
+  if (!csrfToken) return <p>Chargement du formulaire...</p>;
+
+  return <ContactezNous csrfToken={csrfToken} />;
 }
