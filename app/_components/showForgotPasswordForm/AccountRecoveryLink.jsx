cc7dc6d1e356e673bd85a9d15ac0132fef4e5e89
HumanM00n <mrouy92@gmail.com>
2025-06-24 16:57:37 +0200

    UPDATE

--- app/_components/showForgotPasswordForm/AccountRecoveryLink.jsx
+++ app/_components/showForgotPasswordForm/AccountRecoveryLink.jsx
@@ -1,6 +1,16 @@
 'use client';
 
-export default function AccountRecoveryLink({ onClose }) {
+import { useState } from "react";
+
+export default function AccountRecoveryLink({ onClose, onSubmit, }: { onClose: () => void; onSubmit: (email: string) => void; }) {
+  const [ email, setEmail ] = useState("");
+
+
+  const handleSubmit = () => {
+    if (!email.trim()) return;
+    onSubmit(email.trim()); 
+  };
+
   return (
     <>
       <div className="fixed inset-0 bg-black/30 backdrop-blur-sm z-10" onClick={onClose}></div>
@@ -8,7 +18,7 @@ export default function AccountRecoveryLink({ onClose }) {
         <div className="h-[375px] w-96 bg--form rounded-md text-sm shadow-lg relative animate-appear" role="dialog" aria-modal="true"
           onClick={(e) => e.stopPropagation()}>
 
-          <form className="flex flex-col gap-7 items-center p-4">
+          <div onSubmit={handleSubmit} className="flex flex-col gap-7 items-center p-4">
             <div className="relative w-full text-right">
               <button type="button" className="btn-close btn-close-white focus:ring-[#8C5744] focus:border-[#8C5744] focus:ring-2" 
               aria-label="Close" onClick={onClose}></button>
@@ -22,16 +32,22 @@ export default function AccountRecoveryLink({ onClose }) {
               <input
                 type="email"
                 id="inputForEmail"
+                name="emailVerify"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 placeholder="ex : example@gmail.com"
                 required
                 className="form-control md:py-2.5 px-4 placeholder-[#8C5744] placeholder-opacity-70 focus:ring-[#8C5744] focus:border-[#8C5744] focus:ring-4 transition"
               />
             </div>
 
-            <button className="btn btn-outline-light w-[275px] rounded-xs hover:bg-white hover:text-[#733E34] transition" type="submit">
+            <button 
+            className="btn btn-outline-light w-[275px] rounded-xs hover:bg-white hover:text-[#733E34] transition" 
+            onClick={handleSubmit}
+            type="button">
               Continuer
             </button>
-          </form>
+          </div>
         </div>
       </div>
 
