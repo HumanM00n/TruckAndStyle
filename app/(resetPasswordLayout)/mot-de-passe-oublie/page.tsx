d837c44288fead03a56a517df9fb869321b7843c
Mathis Rouy <c1120@cnetp.fr>
2025-06-26 11:30:38 +0200

    Page contenant le composant ui + ajout de metadata pour le titre et la description de la page

---
+++ app/(resetPasswordLayout)/mot-de-passe-oublie/page.tsx
@@ -0,0 +1,10 @@
+import MdpOublie from "@/app/_components/test";
+
+export const metadata = {
+    title: "Réinitialiser votre mot de passe",
+    description: "Saisissez un nouveau mot de passe pour votre compte.",
+};
+
+export default function MdpOubliePage() {
+  return <MdpOublie />
+}
