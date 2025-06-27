980ca0e38d5a9a0c6e6b0a12cff6f2afb3452e7b
Mathis Rouy <c1120@cnetp.fr>
2025-06-27 09:23:33 +0200

    UPDATE

--- app/(resetPasswordLayout)/mot-de-passe-oublie/page.tsx
+++ app/(resetPasswordLayout)/mot-de-passe-oublie/page.tsx
@@ -1,10 +1,10 @@
 import MdpOublie from "@/app/_components/test";
-
 export const metadata = {
     title: "Réinitialiser votre mot de passe",
     description: "Saisissez un nouveau mot de passe pour votre compte.",
 };
 
-export default function MdpOubliePage() {
+export default function Page() {
+
   return <MdpOublie />
 }
