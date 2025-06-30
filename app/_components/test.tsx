1c2f989ad95bd368e677066f6f3bbb8a781809aa
HumanM00n <mrouy92@gmail.com>
2025-06-30 15:18:04 +0200

    Finalisation des testes pour la fonctionnalité du changement de mot de passe

--- app/_components/test.tsx
+++ app/_components/test.tsx
@@ -8,45 +8,91 @@ import { SpinnerTns } from "./defaultComponents/spinner";
 
 export default function MdpOublie() {
   const getTokenByUrl = useSearchParams();
-  const [showPassword, setShowPassword] = useState(false);
+  const [showNewPassword, setShowNewPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [newPassword, setNewPassword] = useState("");
   const [confirmNewPassword, setConfirmNewPassword] = useState("");
   const [isTokenValid, setIsTokenValid] = useState<boolean | null>(null);
   const [isLoading, setIsLoading] = useState(true)
+  const [responseMessage, setResponseMessage] = useState("")
 
   useEffect(() => {
-  const token = getTokenByUrl.get('token');
+    // Vérifie lorsqu'on arrive sur la page 
+    const token = getTokenByUrl.get('token');
 
-  if (!token) {
-    console.log("Aucun token trouvé dans l'url");
-    setIsTokenValid(false);
-    setIsLoading(false);
-    return;
-  }
+    if (!token) {
+      console.log("Aucun token trouvé dans l'url");
+      setIsTokenValid(false);
+      setIsLoading(false);
+      return;
+    }
 
-  const checkToken = async () => {
+    const checkToken = async () => {
+      try {
+        const res = await fetch(`/api/auth/reset-password?token=${token}`);
+        console.log('reponse du status', res.status);
+        
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const data = await res.json();
+
+        if (res.ok) {
+          setIsTokenValid(true);
+        } else {
+          setIsTokenValid(false);
+        }
+      } catch (error) {
+        console.error("Erreur serveur :", error);
+        setIsTokenValid(false);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkToken();
+  }, [getTokenByUrl]);
+
+
+  const handleUpdate = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Permet d'identifier l'utilisateur a mettre à jour
+    const token = getTokenByUrl.get('token');
+
+    if (!token) {
+      setResponseMessage("Lien invalide ou expiré.");
+      return;
+    }
+
+    setIsLoading(true)
+
     try {
-      const res = await fetch(`/api/auth/reset-password?token=${token}`);
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const res = await fetch('api/auth/reset-password', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ newPassword, confirmNewPassword, token }),
+      });
+
       const data = await res.json();
 
-      if (res.ok) {
-        setIsTokenValid(true);
+      if (!res.ok) {
+        setResponseMessage(data.message)
       } else {
-        setIsTokenValid(false);
+        setResponseMessage(data.message)
+
+        setNewPassword("");
+        setConfirmNewPassword("");
       }
+
     } catch (error) {
-      console.error("Erreur serveur :", error);
-      setIsTokenValid(false);
+      console.error('Erreur réseau ou serveur :', error)
+      setResponseMessage("Une erreur est survenue avec le serveur.")
     } finally {
-      setIsLoading(false);
+      setIsLoading(false)
     }
-  };
 
-  checkToken();
-}, [getTokenByUrl]);
-
-
+  }
 
   if (isLoading) {
     return (
@@ -72,7 +118,7 @@ export default function MdpOublie() {
 
   return (
     <section className="h-full w-auto">
-      <form className="flex flex-column justify-center gap-5 pl-5 md:items-center md:pl-0">
+      <form onSubmit={handleUpdate} className="flex flex-column justify-center gap-5 pl-5 md:items-center md:pl-0">
         <div className="mt-1">
           <h1 className="mt-1 text-xl">Réinitialisation de votre de mot de passe</h1>
           <hr className="text-gray-500 opacity-45 px-2" />
@@ -83,11 +129,10 @@ export default function MdpOublie() {
           <p className="italic text-xs opacity-45">Choisissez un mot de passe fort que vous n&apos;avez pas utiliser auparavant</p>
         </div>
 
-
         <div className="flex flex-column gap-2 relative">
           <label htmlFor="inputNewPassword">Mot de passe</label>
           <input
-            type={showPassword ? "text" : "password"}
+            type={showNewPassword ? "text" : "password"}
             className="w-[425px] py-2.5 pl-4 mb-3 bg--form rounded-md placeholder-[#8C5744] focus:border-[#c07a61] focus:ring-2 focus:ring-[#C29A7E] outline-none transition"
             id="inputNewPassword"
             onChange={(e) => setNewPassword(e.target.value)}
@@ -96,16 +141,15 @@ export default function MdpOublie() {
             required />
 
           <FontAwesomeIcon
-            icon={showPassword ? faEye : faEyeSlash}
-            className="absolute right-10 top-[44px] cursor-pointer text-lg text-black"
-            onClick={() => setShowPassword(!showPassword)}
+            icon={showNewPassword ? faEye : faEyeSlash}
+            className="absolute right-10 top-[44px] cursor-pointer text-lg text-black md:right-4"
+            onClick={() => setShowNewPassword(!showNewPassword)}
           />
-
 
           {/* CONFIRMATION DU PASSWORD */}
           <label htmlFor="inputConfirmNewPassword">Confirmation de mot passe</label>
           <input
-            type={showPassword ? "text" : "password"}
+            type={showConfirmPassword ? "text" : "password"}
             className="w-[425px] py-2.5 pl-4 bg--form rounded-md placeholder-[#8C5744] focus:border-[#c07a61] focus:ring-2 focus:ring-[#C29A7E] outline-none transition"
             id="inputConfirmNewPassword"
             onChange={(e) => setConfirmNewPassword(e.target.value)}
@@ -115,18 +159,19 @@ export default function MdpOublie() {
           />
 
           <FontAwesomeIcon
-            icon={newPassword ? faEye : faEyeSlash}
-            className="absolute right-10 top-36 cursor-pointer text-lg text-black"
-            onClick={() => setShowPassword(!showPassword)}
+            icon={confirmNewPassword ? faEye : faEyeSlash}
+            className="absolute right-10 top-36 cursor-pointer text-lg text-black md:right-4"
+            onClick={() => setShowConfirmPassword(!showConfirmPassword)}
           />
         </div>
 
         <div className="flex justify-center">
-          <button className="rounded-md font-base py-2.5 px-4 mb-16 bg--form hover:bg-[#63362d] transition">Enregistrer les modifications</button>
+          <button type="submit" className="rounded-md font-base py-2.5 px-4 mb-16 bg--form hover:bg-[#63362d] transition">Enregistrer les modifications</button>
+          {responseMessage && (
+            <p className="text-sm text-center">{responseMessage}</p>
+          )}
         </div>
-
       </form>
     </section>
   );
-
 }
