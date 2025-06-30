7995803c8f47fa824adc1e64612c8dfab93eec34
HumanM00n <mrouy92@gmail.com>
2025-06-30 15:18:28 +0200

    Modification de l'import du composant ui du mot de passe oublié

--- app/(resetPasswordLayout)/mot-de-passe-oublie/page.tsx
+++ app/(resetPasswordLayout)/mot-de-passe-oublie/page.tsx
@@ -1,10 +1,9 @@
-import MdpOublie from "@/app/_components/test";
+import MdpOublie from "@/app/ui/motDePasseOublie";
 export const metadata = {
     title: "Réinitialiser votre mot de passe",
     description: "Saisissez un nouveau mot de passe pour votre compte.",
 };
 
 export default function Page() {
-
   return <MdpOublie />
 }
