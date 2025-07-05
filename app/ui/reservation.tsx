b8e5c8d3c9af4296f75e49a7b1980ef81d83afaf
HumanM00n <mrouy92@gmail.com>
2025-07-05 16:08:21 +0200

    Ajout du handleSubmit Ajout de la récupération de l'id utilisateur Ajout de handleDateTimeChange

--- app/ui/reservation.tsx
+++ app/ui/reservation.tsx
@@ -4,48 +4,94 @@ import PsChoixCoupes from "../_components/reservation/ps-choixCoupes";
 import DsHoraireAndTruckResa from "../_components/reservation/ds-horaireTruck";
 import TsChoixHeureResa from "../_components/reservation/ts-choixHeure";
 import { View } from "../_lib/choixCoupes.ps-resa";
+import { useSession } from "next-auth/react";
+import { format } from "date-fns";
 
 export default function Reservation() {
   const [coupe, setCoupe] = useState<View | null>('courtes');
   const [coupeChoisie, setCoupeChoisie] = useState<string | null>(null);
 
-  const [date, setDate] = useState<Date | undefined>()
-  const [heure, setHeure] = useState<string | null>(null)
-  const [affichage, setAffichage] = useState<string>("Choisir une date")
+  const [date, setDate] = useState<Date | undefined>(undefined);
+  const [heure, setHeure] = useState<string | null>(null);
+  const [affichage, setAffichage] = useState<string>("Choisir une date");
 
+  const { data: session, status } = useSession();
+  const userId = session?.user.id;
+
+  const handleDateTimeChange = (datetime: string) => {
+    const [dateStr, heureStr] = datetime.split(' ');
+    setDate(new Date(dateStr));
+    setHeure(heureStr.slice(0, 5)); 
+  };
+
+  console.log("email :", session?.user.email);
+  console.log("id :", session?.user.id);
+  console.log(coupeChoisie);
+  console.log(date, heure);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!date || !heure || !coupeChoisie || !userId) {
+      alert("Informations manquantes !");
+      return;
+    }
+
+    const datePart = format(date!, "yyyy-MM-dd");
+    const timePart = heure;
+    const datetime = `${datePart} ${timePart}`;
+
+    console.log("datetime:", datetime);
+
+    try {
+      const res = await fetch("api/booking/", {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({ userId, coupeChoisie, datetime })
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        alert(data.message);
+      } else {
+        alert(data.message);
+        setCoupeChoisie(null);
+        setDate(undefined);
+        setHeure(null);
+        setAffichage("Choisir une date");
+      }
+    } catch (error) {
+      console.error('Erreur réseau ou serveur :', error);
+      alert("Une erreur est survenue avec le serveur.");
+    }
+  };
 
   return (
-    <form className="">
+    <form onSubmit={handleSubmit}>
       <PsChoixCoupes selected={coupe} onSelect={setCoupe} onChoixFinal={setCoupeChoisie} />
       <DsHoraireAndTruckResa />
       <TsChoixHeureResa
+        coupeChoisie={coupeChoisie}
+        setCoupeChoisie={setCoupeChoisie}
         date={date}
         setDate={setDate}
         heure={heure}
         setHeure={setHeure}
         affichage={affichage}
-        setAffichage={setAffichage} />
-
+        setAffichage={setAffichage}
+        onDateTimeChange={handleDateTimeChange}
+      />
       <div className="w-full flex justify-center relative md:bottom-24">
-        <button className="
-		                w-64
-                    py-2.5 
-                    px-6
-                    relative
-                    top-7
-                    mb-16
-                    rounded-md
-                    font-base
-                    bg--form
-                    transition
-                    hover:bg-[#63362d]
-                    md:mb-0
-                    md:top-0
-                    md:left-6
-                    lg:left-9"
-          type="submit" name="">Confirmer</button>
+        <button
+          className="w-64 py-2.5 px-6 relative top-7 mb-16 rounded-md font-base bg--form transition hover:bg-[#63362d] md:mb-0 md:top-0 md:left-6 lg:left-9"
+          type="submit"
+        >
+          Confirmer
+        </button>
       </div>
     </form>
-
   );
 }
