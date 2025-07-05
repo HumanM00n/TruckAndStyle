00d6a70af739f58eac7b9a5ada9a35d6085b6e15
HumanM00n <mrouy92@gmail.com>
2025-07-05 16:58:45 +0200

    Développement en cours de la page suite à la prise de rendez-vous

--- app/(pages)/testDeveloppement/page.tsx
+++ app/(pages)/testDeveloppement/page.tsx
@@ -1,46 +1,18 @@
 'use client';
-
-import Image from "next/image";
-import { dataEquipe, View } from "@/app/_lib/equipeTns";
+import Link from "next/link";
 
 export default function ContactezNous() {
-  const views: View[] = ['CEO', "Conducteur", "Coiffeur"];
+
   return (
-    <section className="min-h-[80vh]">
-
-      <div className="w-full grid grid-cols-3 border-1 border-red-500">
-        {views.map(view => (
-          dataEquipe[view].flatMap(person => (
-            <div
-  key={person.nom + person.prenom + person.photo}
-  className="group relative border border-[#733E34] w-auto px-2 py-3 mt-3 rounded-lg mx-auto cursor-pointer text-sm transition duration-300 hover:scale-105"
->
-  <Image
-    alt=""
-    src={`/assets/Personnel/${person.photo}`}
-    width={150}
-    height={150}
-    quality={70}
-    className="w-36 h-36 mb-3 object-cover rounded-lg lg:w-40 lg:h-40"
-  />
-
-  {/* Overlay visible au hover */}
-  <div className="absolute inset-0 bg-black/60 text-white opacity-0 group-hover:opacity-100 flex items-center justify-center text-center transition duration-300 rounded-lg px-2">
-    {person.informations && (
-      <p className="text-sm">{person.informations}</p>
-    )}
-  </div>
-
-  <div className="text-center">{person.prenom} {person.nom}</div>
-  <div className="text-center">{person.poste}</div>
-</div>
-
-
-
-          ))))}
+    <section>
+      <div className="">
+        <span></span>
+        <p>Votre réservation a bien été pris en compte</p>
       </div>
 
+      <div className="">
+        <Link href={"/profil/"}>Accéder à votre réservation sur votre profil</Link>
+      </div>
     </section>
-
-  );
+)
 }
