2a186b1f2d18a6e39e55cf522280ca7e495ab356
HumanM00n <mrouy92@gmail.com>
2025-07-05 17:43:15 +0200

    Affichage de la confirmation de reservation fonctionnel

--- app/(pages)/testDeveloppement/page.tsx
+++ app/(pages)/testDeveloppement/page.tsx
@@ -1,18 +1,25 @@
 'use client';
 import Link from "next/link";
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 
 export default function ContactezNous() {
 
   return (
-    <section>
-      <div className="">
-        <span></span>
-        <p>Votre réservation a bien été pris en compte</p>
-      </div>
+    <section className="min-h-[80vh] px-2 relative">
+      <div className="bg--form h-64 w-[600px] absolute left-5 top-5 flex flex-col items-center justify-center rounded-md
+      md:left-24
+      lg:w-[650px]
+      lg:left-52
+      lg:top-14
+      xl:left-80">
+        <div className="flex">
+          <CheckCircleOutlineIcon className="text-green-500 relative bottom-2 right-2" sx={{ fontSize: 40 }}/>
+          <h1 className="text-[22px]">Votre réservation a bien été prise en compte !</h1>
+        </div>
 
-      <div className="">
-        <Link href={"/profil/"}>Accéder à votre réservation sur votre profil</Link>
+
+        <Link href={"/profil"} className="underline underline-offset-2 relative top-20 text-sm">Accéder à votre réservation sur votre profil</Link>
       </div>
     </section>
-)
+  )
 }
