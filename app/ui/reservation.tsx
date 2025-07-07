94a3929b8707cc2f6a648513884051a6445c999a
HumanM00n <mrouy92@gmail.com>
2025-07-07 14:53:30 +0200

    Ajout d'un affichage conditionnel en fonction de l'utilisateur s'il est connecté ou non

--- app/ui/reservation.tsx
+++ app/ui/reservation.tsx
@@ -1,10 +1,12 @@
 'use client';
 import { useState } from "react";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
 import PsChoixCoupes from "../_components/reservation/ps-choixCoupes";
 import DsHoraireAndTruckResa from "../_components/reservation/ds-horaireTruck";
 import TsChoixHeureResa from "../_components/reservation/ts-choixHeure";
+import SuccessBooking from "../_components/reservation/objecResa/SuccessRedirect";
 import { View } from "../_lib/choixCoupes.ps-resa";
-import { useSession } from "next-auth/react";
 import { format } from "date-fns";
 
 export default function Reservation() {
@@ -20,12 +22,12 @@ export default function Reservation() {
 
   const [isLoading, setIsLoading] = useState(true)
   const [responseMessage, setResponseMessage] = useState("");
-  const [ isReservationValid, setIsReservationValid ] = useState(false)
+  const [isReservationValid, setIsReservationValid] = useState(false)
 
   const handleDateTimeChange = (datetime: string) => {
     const [dateStr, heureStr] = datetime.split(' ');
     setDate(new Date(dateStr));
-    setHeure(heureStr.slice(0, 5)); 
+    setHeure(heureStr.slice(0, 5));
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -56,11 +58,11 @@ export default function Reservation() {
       if (!res.ok) {
         alert(data.message);
       } else {
-        alert(data.message);
         setCoupeChoisie(null);
         setDate(undefined);
         setHeure(null);
         setAffichage("Choisir une date");
+        setIsReservationValid(true);
       }
     } catch (error) {
       console.error('Erreur réseau ou serveur :', error);
@@ -68,13 +70,41 @@ export default function Reservation() {
     }
   };
 
+
   if (isReservationValid) {
     return (
-
+      <SuccessBooking />
     );
   }
 
-  return (
+  if (status === "authenticated") {
+    return (
+      <form onSubmit={handleSubmit}>
+        <PsChoixCoupes selected={coupe} onSelect={setCoupe} onChoixFinal={setCoupeChoisie} />
+        <DsHoraireAndTruckResa />
+        <TsChoixHeureResa
+          coupeChoisie={coupeChoisie}
+          setCoupeChoisie={setCoupeChoisie}
+          date={date}
+          setDate={setDate}
+          heure={heure}
+          setHeure={setHeure}
+          affichage={affichage}
+          setAffichage={setAffichage}
+          onDateTimeChange={handleDateTimeChange}
+        />
+        <div className="w-full flex justify-center relative md:bottom-24">
+          <button
+            className="w-64 py-2.5 px-6 relative top-7 mb-16 rounded-md font-base bg--form transition hover:bg-[#63362d] md:mb-0 md:top-0 md:left-6 lg:left-9"
+            type="submit"
+          >
+            Confirmer
+          </button>
+        </div>
+      </form>
+    );
+  } else if (status === "unauthenticated")  {
+      return (
     <form onSubmit={handleSubmit}>
       <PsChoixCoupes selected={coupe} onSelect={setCoupe} onChoixFinal={setCoupeChoisie} />
       <DsHoraireAndTruckResa />
@@ -90,13 +120,18 @@ export default function Reservation() {
         onDateTimeChange={handleDateTimeChange}
       />
       <div className="w-full flex justify-center relative md:bottom-24">
-        <button
-          className="w-64 py-2.5 px-6 relative top-7 mb-16 rounded-md font-base bg--form transition hover:bg-[#63362d] md:mb-0 md:top-0 md:left-6 lg:left-9"
-          type="submit"
-        >
-          Confirmer
-        </button>
+        <Link href={"/connexion"} className="w-72 py-2.5 pl-3 relative top-7 mb-16 rounded-md text-sm bg--form transition hover:bg-[#63362d] md:ml-12 md:mb-0 md:top-0 md:left-6
+                    lg:ml-0
+                    lg:text-base
+                    lg:pl-4
+                    lg:w-[340px]
+                    lg:left-9">
+          Veuillez vous connecter pour réserver
+        </Link> 
       </div>
     </form>
   );
+  }
+
+
 }
